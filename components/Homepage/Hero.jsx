93c0126ed4ee775028bb7dd7a508e3c4488705e7
Philani Ncube <ncbphi001@gmail.com>
2022-01-16 09:00:41 +0200

    banner

--- components/Homepage/Hero.jsx
+++ components/Homepage/Hero.jsx
@@ -12,9 +12,7 @@ const Hero = () => {
         <h1 className="font-semibold text-white text-center text-2xl md:text-6xl">
           Photographer based in <br></br> Gqeberha, South Africa{' '}
         </h1>
-<<<<<<< HEAD
-=======
-{/* <Link href="https://wa.me/27783079194" passHref>
+        {/* <Link href="https://wa.me/27783079194" passHref>
           <button
             type="button"
             className="mt-4 bg-amber-400 text-white px-8 py-2 text-center font-medium rounded-full"
@@ -22,7 +20,6 @@ const Hero = () => {
             Learn More
           </button>
         </Link> */}
->>>>>>> c88c59fa66d4f466a6abd71947a87f78a2275ff2
       </div>
     </div>
   );
